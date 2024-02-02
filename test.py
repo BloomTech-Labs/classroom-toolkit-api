9fe6b14f87a0b6b688e7783110103e0a0af4cd67
ashtilawat23 <ashtilawat23@gmail.com>
2024-02-02 14:09:00 -0500

    removed unused code

--- test.py
+++
@@ -1,7 +0,0 @@
-import PyPDF2
-
-# Open the PDF file
-with open('./assets/mit-lesson-plan-template-fillable.pdf', 'rb') as f:
-    reader = PyPDF2.PdfReader(f)
-    fields = reader.get_form_text_fields()
-    print(fields)
